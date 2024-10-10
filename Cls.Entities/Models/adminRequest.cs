bfd63c980a49164bb97d4ebfa787db15aa8c0774
omr746 <112737842+omr746@users.noreply.github.com>
2024-10-10 23:46:26 +0300

    modify admin
    aaa

--- Cls.Entities/Models/adminRequest.cs
+++ Cls.Entities/Models/adminRequest.cs
@@ -11,7 +11,7 @@ namespace Models
         public int? CityId { get; set; }
         public int? CountryId { get; set; }
         public string? PhoneNumber { get; set; }
-        public int? DoctorId { get; set; }    
+        public int DoctorId { get; set; }    
         public Country? Country { get; set; }
         public Doctor? Doctor { get; set; }
         public City? City { get; set; }
