4dacbc3c2a7dbaeec05e711cc3bf4ea07d44c262
Omarmagdy66 <omarmagdy.omar000@gmail.com>
2024-10-11 17:55:45 +0300

    omar

--- Cls.Api/Controllers/ReviewController.cs
+++ Cls.Api/Controllers/ReviewController.cs
@@ -15,10 +15,9 @@ public class ReviewController : APIBaseController
     [HttpGet]
     public async Task<IActionResult> GetAllReviews()
     {
-        //var Reviews = await _unitOfWork.Reviews.GetAllAsync();
         return Ok(await _unitOfWork.Reviews.GetAllAsync());
     }
-    [HttpGet("{id}")]
+    [HttpGet("GetReviewById")]
     public async Task<IActionResult> GetReviewById(int id)
     {
         var Review = await _unitOfWork.Reviews.GetByIdAsync(id);
@@ -28,7 +27,7 @@ public class ReviewController : APIBaseController
         }
         return Ok(Review);
     }
-    [HttpPost]
+    [HttpPost("AddReview")]
     public async Task<IActionResult> AddReview(ReviewDto reviewdto)
     {
         if (ModelState.IsValid)
@@ -47,7 +46,7 @@ public class ReviewController : APIBaseController
         }
         return BadRequest($"ther are {ModelState.ErrorCount} errors");
     }
-    [HttpPut("{id}")]
+    [HttpPut("EditReview")]
     public async Task<IActionResult> EditReview(int id, [FromBody] ReviewDto reviewdto)
     {
         var Review = await _unitOfWork.Reviews.GetByIdAsync(id);
@@ -62,14 +61,14 @@ public class ReviewController : APIBaseController
             Review.Rating = reviewdto.Rating;
             Review.ReviewText = reviewdto.ReviewText;
             Review.ReviewDate = reviewdto.ReviewDate;
-				_unitOfWork.Reviews.Update(Review);
+			_unitOfWork.Reviews.Update(Review);
             _unitOfWork.Save();
-            return Ok("Updated!");
+            return Ok("Updated Sucsessfully");
         }
         return BadRequest($"There are {ModelState.ErrorCount}");
     }
 
-    [HttpDelete("{id}")]
+    [HttpDelete("DeleteReview")]
     public async Task<IActionResult> DeleteReview(int id)
     {
         var Review = await _unitOfWork.Reviews.GetByIdAsync(id);
@@ -79,6 +78,88 @@ public class ReviewController : APIBaseController
         }
         _unitOfWork.Reviews.Delete(Review);
         _unitOfWork.Save();
-        return Ok("Deleted!");
+        return Ok("Deleted Sucsessfuly");
     }
+    [HttpGet("GetReviewsForDoctor")]
+    public async Task<IActionResult> GetReviewsForDoctor(int doctorId)
+    {
+        var reviews = await _unitOfWork.Reviews.FindAllAsync(r => r.DoctorId == doctorId);
+        return Ok(reviews);
+    }
+    [HttpGet("GetAverageRatingForDoctor")]
+    public async Task<IActionResult> GetAverageRatingForDoctor(int doctorId)
+    {
+        var reviews = await _unitOfWork.Reviews.FindAllAsync(r => r.DoctorId == doctorId);
+        var averageRating = reviews.Average(r => r.Rating);
+        return Ok(new { averageRating });
+    }
+    [HttpGet("GetReviewsForUser")]
+    public async Task<IActionResult> GetReviewsForUser(int patientId)
+    {
+        var reviews = await _unitOfWork.Reviews.FindAllAsync(r => r.PatientId == patientId);
+        return Ok(reviews);
+    }
+
+
+    //[HttpPost("FlagReview")]
+    //public async Task<IActionResult> FlagReview(int reviewId, [FromBody] string reason)
+    //{
+    //    var review = await _unitOfWork.Reviews.GetByIdAsync(reviewId);
+    //    if (review == null)
+    //        return NotFound();
+
+    //    review.IsFlagged = true;
+    //    review.FlagReason = reason;
+    //    _unitOfWork.Reviews.Update(review);
+    //    _unitOfWork.Save();
+
+    //    return Ok(new { message = "Review flagged." });
+    //}
+
+    [HttpGet("CheckUserReviewedDoctor")]
+    public  IActionResult HasUserReviewedDoctor(int patientId, int doctorId)
+    {
+        var hasReviewed =  _unitOfWork.Reviews.IsExist(r => r.PatientId == patientId && r.DoctorId == doctorId);
+
+        return Ok(new { hasReviewed });
+    }
+    //[HttpGet("flagged")]
+    //public async Task<IActionResult> GetFlaggedReviews()
+    //{
+    //    var flaggedReviews = await _unitOfWork.Reviews.FindAllAsync(r => r.IsFlagged);
+    //    return Ok(flaggedReviews);
+    //}
+
+    //[HttpPut("ReviewFlagAction")]
+    //public async Task<IActionResult> ReviewFlagAction(int reviewId, [FromBody] string action)
+    //{
+    //    var review = await _unitOfWork.Reviews.GetByIdAsync(reviewId);
+    //    if (review == null)
+    //        return NotFound();
+
+    //    if (action.ToLower() == "approve")
+    //    {
+    //        review.IsApproved = true;
+    //    }
+    //    else if (action.ToLower() == "reject")
+    //    {
+    //        review.IsFlagged = false;
+    //        review.FlagReason = null;
+    //    }
+
+    //    _unitOfWork.Reviews.Update(review);
+    //     _unitOfWork.Save();
+
+    //    return Ok(new { message = "Review flag action processed." });
+    //}
+
+
+
+
+
+
+
+
+
+
 }
--- Cls.Entities/Models/Review.cs
+++ Cls.Entities/Models/Review.cs
@@ -8,7 +8,11 @@ public partial class Review
     public int DoctorId { get; set; }
     public int Rating { get; set; }
     public string? ReviewText { get; set; }
-    public DateOnly? ReviewDate { get; set; }
+    public DateTime? ReviewDate { get; set; }
+    //public bool IsFlagged { get; set; } = false;
+    //public string? FlagReason { get; set; }
+    //public bool IsApproved { get; set; } = true;
+    //public int HelpfulCount { get; set; } = 0;
 
     public Patient? Patient { get; set; } 
     public Doctor? Doctor { get; set; } 
