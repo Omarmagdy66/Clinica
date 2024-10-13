988f50a89df6d9a7585e84ced5fd85f67574318a
sherif <sherifelgendy2004@gmail.com>
2024-10-13 09:39:19 -0700

    finalsEveLoginDoneo

---
+++ Cls.DAL/Migrations/20241013065303___.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cls.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class __ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
